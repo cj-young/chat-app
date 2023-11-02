7d4dafeb3c30e095c210484faa651281742b06c2
CJ Young <chrjyoung@gmail.com>
2023-11-01 23:40:50 -0400

    Add next auth types

---
+++ types/next-auth.d.ts
@@ -0,0 +1,61 @@
+import { DefaultSession, DefaultUser } from "next-auth";
+import { JWT, DefaultJWT } from "next-auth/jwt";
+
+interface VerifiedUser {
+  username: string;
+  displayName: string;
+  email: string;
+  imageURL: string;
+  id: string;
+  verified: boolean;
+}
+
+interface UnverifiedUser {
+  id: string;
+  email: string;
+  verified: boolean;
+}
+
+interface test {
+  username?: string;
+  displayName?: string;
+  email: string;
+  imageURL?: string;
+  id: string;
+  verified: boolean;
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      username?: string;
+      displayName?: string;
+      email: string;
+      imageURL?: string;
+      id: string;
+      verified: boolean;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    username?: string;
+    displayName?: string;
+    email: string;
+    imageURL?: string;
+    id: string;
+    verified: boolean;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    user?: {
+      username?: string;
+      displayName?: string;
+      email: string;
+      imageURL?: string;
+      id: string;
+      verified: boolean;
+    };
+  }
+}
