3bfb476172f32941a670bc383504cdb73eb0008d
CJ Young <chrjyoung@gmail.com>
2023-11-02 20:44:57 -0400

    Add back button to second stage of sign up

--- app/(auth)/signup/CreateNameForm.tsx
+++ app/(auth)/signup/CreateNameForm.tsx
@@ -1,15 +1,29 @@
 "use client";
-
 import styles from "./styles.module.scss";
 import ErrorSymbol from "@/public/triangle-exclamation-solid.svg";
+import { signOut } from "next-auth/react";
 import { useState } from "react";
+import BackArrow from "@/public/left-long-solid.svg";
 
 export default function CreateNameForm() {
   const [error, setError] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [username, setUsername] = useState("");
+
+  async function goBack() {
+    await signOut({ redirect: false });
+  }
+
   return (
     <form className={styles["sign-up-form"]}>
+      <button
+        aria-label="back"
+        type="button"
+        onClick={goBack}
+        className={styles.back}
+      >
+        <BackArrow alt="Go back" /> Back
+      </button>
       <input
         type="text"
         placeholder="Display name"
