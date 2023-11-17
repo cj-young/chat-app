0aeb51274e7d05afb4d55a69501bf75111952b90
CJ Young <chrjyoung@gmail.com>
2023-11-16 20:17:09 -0500

    Create unverified user session constructor

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,11 @@
 import Session from "@/models/Session";
 import { IUser } from "@/models/User";
 import dbConnect from "./dbConnect";
+import { IUnverifiedUser } from "@/models/UnverifiedUser";
+import SignupSession from "@/models/SignupSession";
 
 const EXPIRY_TIME = 1000 * 60 * 60 * 24 * 5; // 5 days
+const SIGNUP_EXPIRY_TIME = 1000 * 60 * 60; // 1 hour
 
 export async function createSession(user: IUser) {
   await dbConnect();
@@ -10,6 +13,19 @@ export async function createSession(user: IUser) {
   const expiresAt = new Date(Date.now() + EXPIRY_TIME);
 
   const session = await Session.create({
+    user: user.id,
+    expiresAt
+  });
+
+  return session;
+}
+
+export async function createSignupSession(user: IUnverifiedUser) {
+  await dbConnect();
+
+  const expiresAt = new Date(Date.now() + SIGNUP_EXPIRY_TIME);
+
+  const session = await SignupSession.create({
     user: user.id,
     expiresAt
   });
