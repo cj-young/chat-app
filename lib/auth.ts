4b70b09283f63e07aec42bc56e73dc972cca372d
CJ Young <chrjyoung@gmail.com>
2023-11-18 23:35:24 -0500

    Remove unnecessary expiry time constant

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,6 @@
 import Session from "@/models/Session";
 import { IUser } from "@/models/User";
 import dbConnect from "./dbConnect";
-const SIGNUP_EXPIRY_TIME = 1000 * 60 * 60; // 1 hour
 
 export async function createSession(user: IUser) {
   await dbConnect();
