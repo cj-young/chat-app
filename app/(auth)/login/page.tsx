ccc64e69fd818f4de949978e33f67bba2d7a90b4
CJ Young <chrjyoung@gmail.com>
2023-11-19 00:02:50 -0500

    Add redirect after successful login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,29 +5,49 @@ import Link from "next/link";
 import GoogleLogo from "@/public/google-logo.svg";
 import ErrorSymbol from "@/public/triangle-exclamation-solid.svg";
 import { apiFetch } from "@/lib/api";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
   const [error, setError] = useState("");
   const [identifier, setIdentifier] = useState("");
   const [password, setPassword] = useState("");
 
+  const router = useRouter();
+
+  async function logIn() {
+    try {
+      const res = await apiFetch("/auth/login", "POST", {
+        identifier,
+        password
+      });
+
+      if (!res.ok) {
+        try {
+          const data = await res.json();
+          setError(data.message);
+        } catch (error) {
+          console.log("asdasdsadasd");
+          setError("An error occurred, please try again");
+        } finally {
+          return false;
+        }
+      }
+
+      const data = await res.json();
+      return true;
+    } catch (error) {
+      setError("An error occurred, please try again");
+      throw error;
+    }
+  }
+
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    const res = await apiFetch("/auth/login", "POST", { identifier, password });
-
-    if (!res.ok) {
-      try {
-        const data = await res.json();
-        setError(data.message);
-      } catch (error) {
-        setError("An error occurred, please try again");
-      } finally {
-        return;
-      }
+    const loginSuccess = await logIn();
+    if (loginSuccess) {
+      router.push("/");
     }
-
-    const data = await res.json();
   }
 
   return (
