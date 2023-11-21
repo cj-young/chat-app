2b4abfc234d646bcaf450cf220841af822c97cd9
CJ Young <chrjyoung@gmail.com>
2023-11-20 22:43:41 -0500

    Create functions to retrieve session info

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
-import Session from "@/models/Session";
+import Session, { ISession } from "@/models/Session";
 import { IUser } from "@/models/User";
 import dbConnect from "./dbConnect";
+import SignupSession, { ISignupSession } from "@/models/SignupSession";
 
 export async function createSession(user: IUser) {
   await dbConnect();
@@ -10,4 +11,17 @@ export async function createSession(user: IUser) {
   });
 
   return session;
+}
+
+export async function getSessionUser(sessionId: string) {
+  const session = await Session.findById<ISession>(sessionId).populate<{
+    user: IUser;
+  }>("user");
+
+  return !session || session.isExpired() ? null : session.user;
+}
+
+export async function getSignupSession(sessionId: string) {
+  const session = await SignupSession.findById<ISignupSession>(sessionId);
+  return !session || session.isExpired() ? null : session;
 }
