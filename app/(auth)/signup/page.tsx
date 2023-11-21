145ae0f4af7f1daa71de2d2131bd1a0e227a15b8
CJ Young <chrjyoung@gmail.com>
2023-11-21 18:11:22 -0500

    Fix session not being properly obtained

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -2,14 +2,18 @@ import styles from "./styles.module.scss";
 import FormWrapper from "./forms/FormWrapper";
 import { cookies } from "next/headers";
 import { getSignupSession } from "@/lib/auth";
+import dbConnect from "@/lib/dbConnect";
 
 export default async function SignUp() {
   let authStatus: "unauthenticated" | "signingUp";
 
   try {
     const sessionId = cookies().get("session")?.value;
+
+    await dbConnect();
+
     if (sessionId && sessionId[0] === "0") {
-      const session = await getSignupSession(sessionId);
+      const session = await getSignupSession(sessionId.slice(1));
 
       if (!session) {
         authStatus = "unauthenticated";
