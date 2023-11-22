bbb2dec01f805c3088217b6d95dbaf790bf27f14
CJ Young <chrjyoung@gmail.com>
2023-11-21 21:06:22 -0500

    Add cookie setter to name creation API route

--- app/api/auth/create-name/route.ts
+++ app/api/auth/create-name/route.ts
@@ -1,3 +1,6 @@
+import { createSession } from "@/lib/auth";
+import dbConnect from "@/lib/dbConnect";
+import { SESSION_EXPIRY_SECONDS } from "@/models/Session";
 import SignupSession, { ISignupSession } from "@/models/SignupSession";
 import User, { IUser } from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
@@ -14,6 +17,8 @@ export async function POST(req: NextRequest) {
     if (!sessionId || sessionId[0] !== "0") {
       return NextResponse.json({ message: "Invalid session" }, { status: 401 });
     }
+
+    await dbConnect();
 
     const session = await SignupSession.findById<ISignupSession>(
       sessionId.slice(1)
@@ -42,7 +47,22 @@ export async function POST(req: NextRequest) {
       email: session.email
     });
 
-    return NextResponse.json({ message: "Successfully signed up" });
+    const newSession = await createSession(user);
+
+    const res = NextResponse.json({ message: "Successfully signed up" });
+
+    res.cookies.set({
+      name: "session",
+      value: "1" + newSession.id, // Prefix verified user session with "1"
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * SESSION_EXPIRY_SECONDS),
+      sameSite: "lax"
+    });
+
+    SignupSession.findByIdAndDelete(sessionId.slice(1));
+    // No need to await deletion, the document will expire anyway if it fails
+
+    return res;
   } catch (error) {
     console.error(error);
     return NextResponse.json(
