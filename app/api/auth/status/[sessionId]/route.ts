fcded393756703a60424e1aa65f589394d549093
CJ Young <chrjyoung@gmail.com>
2023-11-21 21:10:31 -0500

    Add try/catch to API route

--- app/api/auth/status/[sessionId]/route.ts
+++ app/api/auth/status/[sessionId]/route.ts
@@ -5,23 +5,33 @@ import SignupSession, { ISignupSession } from "@/models/SignupSession";
 import dbConnect from "@/lib/dbConnect";
 
 export async function GET(req: NextRequest) {
-  await dbConnect();
+  try {
+    await dbConnect();
 
-  const sessionParam = req.url.slice(req.url.lastIndexOf("/") + 1);
+    const sessionParam = req.url.slice(req.url.lastIndexOf("/") + 1);
 
-  const sessionId = sessionParam.slice(1);
-  const sessionTag = sessionParam[0];
+    const sessionId = sessionParam.slice(1);
+    const sessionTag = sessionParam[0];
 
-  if (sessionTag === "1") {
-    const session = (await Session.findById(sessionId)) as ISession;
-    if (session && !session.isExpired()) {
-      return NextResponse.json({ authStatus: "authenticated" });
+    if (sessionTag === "1") {
+      const session = (await Session.findById(sessionId)) as ISession;
+      if (session && !session.isExpired()) {
+        return NextResponse.json({ authStatus: "authenticated" });
+      }
+    } else if (sessionTag === "0") {
+      const session = (await SignupSession.findById(
+        sessionId
+      )) as ISignupSession;
+      if (session && !session.isExpired()) {
+        return NextResponse.json({ authStatus: "signingUp" });
+      }
     }
-  } else if (sessionTag === "0") {
-    const session = (await SignupSession.findById(sessionId)) as ISignupSession;
-    if (session && !session.isExpired()) {
-      return NextResponse.json({ authStatus: "signingUp" });
-    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
   }
 
   return NextResponse.json({ authStatus: "unauthenticated" });
