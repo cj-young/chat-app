fe39c1da14e06afbf7f421396136ce9b64fe6e5d
CJ Young <chrjyoung@gmail.com>
2023-11-23 21:25:38 -0500

    Add password visibility toggle on input component

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,22 +1,45 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, HTMLInputTypeAttribute, useState } from "react";
 import styles from "./styles.module.scss";
 import ErrorSymbol from "@/public/triangle-exclamation-solid.svg";
+import EyeSlash from "@/public/eye-slash-solid.svg";
+import Eye from "@/public/eye-solid.svg";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  type: string;
+  type: HTMLInputTypeAttribute;
   error?: string;
 }
 
 export default function index({ type, className, error, ...rest }: Props) {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  function togglePasswordVisibility() {
+    setPasswordVisible((prevPasswordVisible) => !prevPasswordVisible);
+  }
+
   return (
-    <div className={[styles["input-wrapper"], className].join(" ")}>
-      <input
-        type={type}
-        className={[styles["input"], error ? styles["input-error"] : ""].join(
-          " "
+    <div className={[styles["input-container"], className].join(" ")}>
+      <div
+        className={[
+          styles["input-wrapper"],
+          error ? styles["input-error"] : ""
+        ].join(" ")}
+      >
+        <input
+          type={type === "password" && passwordVisible ? "text" : type}
+          className={styles["input"]}
+          {...rest}
+        />
+        {type === "password" && (
+          <button
+            className={styles["toggle-password"]}
+            type="button"
+            onClick={togglePasswordVisibility}
+            aria-label={`${passwordVisible ? "Hide" : "Show"} password`}
+          >
+            {passwordVisible ? <EyeSlash /> : <Eye />}
+          </button>
         )}
-        {...rest}
-      />
+      </div>
       {error && (
         <div className={styles["error"]}>
           <ErrorSymbol />
