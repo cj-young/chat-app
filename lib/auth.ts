d70df2d57af929e84bd1fb4e9d1e2f53678882aa
CJ Young <chrjyoung@gmail.com>
2023-12-05 19:59:15 -0500

    Change getSessionUser to return populated user

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import Session, { ISession } from "@/models/Session";
 import SignupSession, { ISignupSession } from "@/models/SignupSession";
-import { IUser } from "@/models/User";
+import User, { IUser } from "@/models/User";
 import dbConnect from "./dbConnect";
 
 export async function createSession(user: IUser): Promise<ISession> {
@@ -15,9 +15,17 @@ export async function createSession(user: IUser): Promise<ISession> {
 
 export async function getSessionUser(sessionId: string) {
   await dbConnect();
+
   const session = await Session.findById<ISession>(sessionId).populate<{
-    user: IUser;
-  }>("user");
+    user: Omit<IUser, "friends" | "friendRequests"> & {
+      friends: IUser[];
+      friendRequests: IUser[];
+    };
+  }>({
+    path: "user",
+    model: User,
+    populate: ["friends", "friendRequests"]
+  });
 
   return !session || session.isExpired() ? null : session.user;
 }
@@ -26,4 +34,15 @@ export async function getSignupSession(sessionId: string) {
   await dbConnect();
   const session = await SignupSession.findById<ISignupSession>(sessionId);
   return !session || session.isExpired() ? null : session;
+}
+
+export function getUserProfile(
+  user: Pick<IUser, "email" | "username" | "displayName" | "imageUrl">
+) {
+  return {
+    email: user.email,
+    username: user.username,
+    displayName: user.displayName,
+    imageUrl: user.imageUrl
+  };
 }
