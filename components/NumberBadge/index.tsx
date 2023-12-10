89772ccd58573302af689d14f50a8fc59ec2760a
CJ Young <chrjyoung@gmail.com>
2023-12-10 11:28:07 -0500

    Add className prop to number badge component

--- components/NumberBadge/index.tsx
+++ components/NumberBadge/index.tsx
@@ -2,17 +2,20 @@ import styles from "./styles.module.scss";
 
 interface Props {
   number: number;
+  className?: string;
 }
 
-export default function NumberBadge({ number }: Props) {
+export default function NumberBadge({ number, className }: Props) {
   const display =
     number <= 0 ? "0" : number < 1000 ? number.toString() : "999+";
 
   return (
     <div
-      className={[styles[`length-${display.length}`], styles["badge"]].join(
-        " "
-      )}
+      className={[
+        styles[`length-${display.length}`],
+        styles["badge"],
+        className
+      ].join(" ")}
     >
       {display}
     </div>
