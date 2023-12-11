1c088f5417dc23953f40391564c7d34c80559908
CJ Young <chrjyoung@gmail.com>
2023-12-11 17:06:15 -0500

    Add message components to chat component

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -1,5 +1,32 @@
+import Message from "@/components/Message";
+import { IClientMessage } from "@/types/user";
 import styles from "./styles.module.scss";
 
-export default function Chat() {
-  return <div className={styles["chat-container"]}></div>;
+interface Props {
+  initialMessages: IClientMessage[];
+}
+
+export default function Chat({ initialMessages }: Props) {
+  return (
+    <ul className={styles["chat-container"]}>
+      {initialMessages.map((message, i) => {
+        let isInGroup = false;
+        if (i < initialMessages.length - 1) {
+          isInGroup =
+            initialMessages[i].sender.id === initialMessages[i + 1].sender.id &&
+            initialMessages[i].timestamp.getTime() -
+              initialMessages[i + 1].timestamp.getTime() <
+              5 * 60 * 1000; // True if messages are sent within 5 minutes
+        }
+
+        return (
+          <Message
+            message={message}
+            isFirst={!isInGroup}
+            key={message.timestamp.toString()}
+          />
+        );
+      })}
+    </ul>
+  );
 }
