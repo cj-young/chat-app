20743f21dedc596118f6c3884405ee77767be124
CJ Young <chrjyoung@gmail.com>
2023-12-12 23:12:23 -0500

    Fix mobile nav not taking up full screen in DM

--- app/(main)/dm/[dmChatId]/components/DmChatContainer/index.tsx
+++ app/(main)/dm/[dmChatId]/components/DmChatContainer/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Chat from "@/components/Chat";
 import ChatInput from "@/components/ChatInput";
+import { useUiContext } from "@/contexts/UiContext";
 import { IClientDm, IClientMessage } from "@/types/user";
 import DmNavbar from "../DmNavbar";
 import styles from "./styles.module.scss";
@@ -11,13 +12,20 @@ interface Props {
   allLoaded: boolean;
 }
 
-export default async function DmChatContainer({
+export default function DmChatContainer({
   directMessageChat,
   messages,
   allLoaded
 }: Props) {
+  const { mobileNavExpanded } = useUiContext();
+
   return (
-    <main className={styles["chat-page-container"]}>
+    <main
+      className={[
+        styles["chat-page-container"],
+        mobileNavExpanded ? styles["hidden"] : ""
+      ].join(" ")}
+    >
       <DmNavbar directMessageChat={directMessageChat} />
       <Chat
         initialMessages={messages}
