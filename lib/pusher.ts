cd7b04d3a303e935e7ed1171dcb41afcc99707e2
CJ Young <chrjyoung@gmail.com>
2023-12-14 18:58:34 -0500

    Fix extra websocket connections being made by backend

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,18 +1,31 @@
 import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
-export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID as string,
-  key: process.env.NEXT_PUBLIC_PUSHER_KEY as string,
-  secret: process.env.PUSHER_SECRET as string,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
-  useTLS: true
-});
+declare global {
+  var pusherServer: PusherServer; // This must be a `var` and not a `let / const`
+  var pusherClient: PusherClient;
+}
 
-export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_KEY as string,
-  {
+if (!global.pusherServer) {
+  global.pusherServer = new PusherServer({
+    appId: process.env.PUSHER_APP_ID as string,
+    key: process.env.NEXT_PUBLIC_PUSHER_KEY as string,
+    secret: process.env.PUSHER_SECRET as string,
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
-    authEndpoint: "/api/pusher/auth"
-  }
-);
+    useTLS: true
+  });
+}
+
+export const pusherServer = global.pusherServer;
+
+if (!global.pusherClient) {
+  global.pusherClient = new PusherClient(
+    process.env.NEXT_PUBLIC_PUSHER_KEY as string,
+    {
+      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
+      authEndpoint: "/api/pusher/auth"
+    }
+  );
+}
+
+export const pusherClient = global.pusherClient;
