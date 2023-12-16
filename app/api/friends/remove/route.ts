c35dbfbc398fbfe4e1f17bfd1688ed936f871123
CJ Young <chrjyoung@gmail.com>
2023-12-16 16:37:21 -0500

    Add friend removal route

---
+++ app/api/friends/remove/route.ts
@@ -0,0 +1,32 @@
+import { getSession, invalidSession } from "@/lib/auth";
+import User, { IUser } from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const sessionId = req.cookies.get("session")?.value;
+    if (!sessionId) return invalidSession();
+
+    const { query, userType } = getSession(sessionId);
+    if (userType !== "verified") return invalidSession();
+
+    const session = await query.populate<{ user: IUser }>("user");
+    if (!session?.user) return invalidSession();
+
+    const { user } = session;
+
+    const { receiverId } = (await req.json()) as { receiverId: string };
+    await Promise.all([
+      User.findByIdAndUpdate(user.id, { $pull: { friends: receiverId } }),
+      User.findByIdAndUpdate(receiverId, { $pull: { friends: user.id } })
+    ]);
+
+    return NextResponse.json({ message: "Successfully removed friend" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
