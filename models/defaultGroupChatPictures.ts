a6c9c4798f9877ed8fce7a2148a96ded21263c40
CJ Young <chrjyoung@gmail.com>
2023-12-17 17:09:32 -0500

    Add default group chat pictures

---
+++ models/defaultGroupChatPictures.ts
@@ -0,0 +1,9 @@
+export default [
+  "https://firebasestorage.googleapis.com/v0/b/next-chat-app-efcab.appspot.com/o/group-chat-pictures%2Fblue.png?alt=media&token=aa249259-2d4c-449f-ae15-b090afe97d31",
+  "https://firebasestorage.googleapis.com/v0/b/next-chat-app-efcab.appspot.com/o/group-chat-pictures%2Fcyan.png?alt=media&token=c0ba4450-0bc5-4bd5-9ed1-96b5585d736b",
+  "https://firebasestorage.googleapis.com/v0/b/next-chat-app-efcab.appspot.com/o/group-chat-pictures%2Fgreen.png?alt=media&token=d69cd7e9-b5d7-429d-b71f-dc4fb01bc505",
+  "https://firebasestorage.googleapis.com/v0/b/next-chat-app-efcab.appspot.com/o/group-chat-pictures%2Fmagenta.png?alt=media&token=b41078ec-b9c3-4cb8-aa90-18c4991cceae",
+  "https://firebasestorage.googleapis.com/v0/b/next-chat-app-efcab.appspot.com/o/group-chat-pictures%2Forange.png?alt=media&token=f996d66c-b845-41e7-9c51-7598bbb6d21c",
+  "https://firebasestorage.googleapis.com/v0/b/next-chat-app-efcab.appspot.com/o/group-chat-pictures%2Fred.png?alt=media&token=9d2a3c71-11b0-47e7-ab29-4b2d1352dd1c",
+  "https://firebasestorage.googleapis.com/v0/b/next-chat-app-efcab.appspot.com/o/group-chat-pictures%2Fyellow.png?alt=media&token=b9649964-70d3-4bab-8d62-6fe317fc5832"
+];
