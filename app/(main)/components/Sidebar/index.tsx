3e06077c70411f8b17ba5912247ab29b29379370
CJ Young <chrjyoung@gmail.com>
2023-12-17 20:40:12 -0500

    Add display of group chats on sidebar

--- app/(main)/components/Sidebar/index.tsx
+++ app/(main)/components/Sidebar/index.tsx
@@ -5,15 +5,30 @@ import { useUiContext } from "@/contexts/UiContext";
 import PlusSymbol from "@/public/plus-solid.svg";
 import FriendsIcon from "@/public/user-group-solid.svg";
 import Link from "next/link";
+import { useMemo } from "react";
 import AddConversationModal from "../AddConversationModal";
 import DirectMessageItem from "./components/DirectMessageItem";
+import GroupChatItem from "./components/GroupChatItem";
 import styles from "./styles.module.scss";
 
 export default function Sidebar() {
   const { mobileNavExpanded } = useUiContext();
 
-  const { friendRequests, directMessages } = useAuthContext();
+  const { friendRequests, directMessages, groupChats } = useAuthContext();
   const { addModal } = useUiContext();
+
+  const allConversations = useMemo(() => {
+    const taggedDirectMessages = directMessages.map((directMessage) => ({
+      ...directMessage,
+      type: "directMessage" as const
+    }));
+    const taggedGroupChats = groupChats.map((groupChat) => ({
+      ...groupChat,
+      type: "groupChat" as const
+    }));
+
+    return [...taggedDirectMessages, ...taggedGroupChats];
+  }, [directMessages, groupChats]);
 
   return (
     <div
@@ -41,13 +56,17 @@ export default function Sidebar() {
           <PlusSymbol />
         </button>
         <ul className={styles["conversations-list"]}>
-          {directMessages
+          {allConversations
             .sort(
               (a, b) => b.lastMessageAt.getTime() - a.lastMessageAt.getTime()
             )
-            .map((dm) => (
-              <DirectMessageItem directMessage={dm} key={dm.chatId} />
-            ))}
+            .map((chat) =>
+              chat.type === "directMessage" ? (
+                <DirectMessageItem directMessage={chat} key={chat.chatId} />
+              ) : (
+                <GroupChatItem groupChat={chat} key={chat.chatId} />
+              )
+            )}
         </ul>
       </nav>
     </div>
