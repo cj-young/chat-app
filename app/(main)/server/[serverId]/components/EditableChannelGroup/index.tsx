51bfb14f6b4e00f0bb4bd316c445fa49a191db8d
CJ Young <chrjyoung@gmail.com>
2024-01-03 23:20:51 -0500

    Add drag and drop to server sidebar channels and groups

--- app/(main)/server/[serverId]/components/EditableChannelGroup/index.tsx
+++ app/(main)/server/[serverId]/components/EditableChannelGroup/index.tsx
@@ -4,6 +4,8 @@ import CaretIcon from "@/public/caret-down-solid.svg";
 import GripIcon from "@/public/grip-solid.svg";
 import PlusSymbol from "@/public/plus-solid.svg";
 import { IClientChannelGroup } from "@/types/server";
+import { SortableContext, useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { useMemo, useState } from "react";
 import AddChannelModal from "../AddChannelModal";
 import EditableChannelItem from "../EditableChannelItem";
@@ -24,12 +26,44 @@ export default function EditableChannelGroup({
     return channels.sort((a, b) => a.uiOrder - b.uiOrder);
   }, [channels]);
 
+  const channelIds = useMemo(() => {
+    return sortedChannels.map((channel) => channel.channelId);
+  }, [sortedChannels]);
+
   function handleExpand() {
     setIsExpanded((prev) => !prev);
   }
 
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+    setActivatorNodeRef
+  } = useSortable({
+    id: groupId,
+    data: {
+      type: "channelGroup"
+    },
+    animateLayoutChanges: () => false
+  });
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    transition
+  };
+
   return (
-    <li className={styles["channel-group"]}>
+    <li
+      className={[
+        styles["channel-group"],
+        isDragging ? styles["being-dragged"] : ""
+      ].join(" ")}
+      ref={setNodeRef}
+      style={style}
+    >
       <div className={styles["group-name-container"]} onClick={handleExpand}>
         <button
           className={[
@@ -43,19 +77,20 @@ export default function EditableChannelGroup({
         <div
           className={styles["move-group-grip"]}
           onClick={(e) => e.preventDefault()}
+          ref={setActivatorNodeRef}
+          {...attributes}
+          {...listeners}
         >
           <GripIcon />
         </div>
       </div>
       <ul className={styles["channel-list"]}>
-        {isExpanded &&
-          sortedChannels.map((channel) => (
-            <EditableChannelItem
-              channel={channel}
-              key={channel.channelId}
-              id={channel.channelId}
-            />
-          ))}
+        <SortableContext items={channelIds}>
+          {isExpanded &&
+            sortedChannels.map((channel) => (
+              <EditableChannelItem channel={channel} key={channel.channelId} />
+            ))}
+        </SortableContext>
         <li className={styles["add-channel-container"]}>
           <button
             className={styles["add-channel"]}
--- app/(main)/server/[serverId]/components/EditableSidebar/index.tsx
+++ app/(main)/server/[serverId]/components/EditableSidebar/index.tsx
@@ -9,9 +9,21 @@ import { apiFetch } from "@/lib/api";
 import PlusSymbol from "@/public/plus-solid.svg";
 import { IClientChannel, IClientChannelGroup } from "@/types/server";
 import { IProfile } from "@/types/user";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverEvent,
+  DragOverlay,
+  DragStartEvent
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy
+} from "@dnd-kit/sortable";
 import { useEffect, useMemo, useState } from "react";
 import AddGroupModal from "../AddGroupModal";
 import EditableChannelGroup from "../EditableChannelGroup";
+import EditableChannelItem from "../EditableChannelItem";
 import ServerCard from "../ServerCard";
 import styles from "./styles.module.scss";
 
@@ -20,10 +32,20 @@ export default function EditableSidebar() {
   const { role, serverInfo } = useServer();
   const { mobileNavExpanded, addModal } = useUiContext();
   const { call } = useVoiceCall();
+  const [draggedGroup, setDraggedGroup] = useState<IClientChannelGroup | null>(
+    null
+  );
+  const [draggedChannel, setDraggedChannel] = useState<IClientChannel | null>(
+    null
+  );
 
   const sortedChannelGroups = useMemo(() => {
     return channelGroups.sort((a, b) => a.uiOrder - b.uiOrder);
   }, [channelGroups]);
+
+  const groupIds = useMemo(() => {
+    return sortedChannelGroups.map((group) => group.id);
+  }, [sortedChannelGroups]);
 
   useEffect(() => {
     (async () => {
@@ -150,6 +172,171 @@ export default function EditableSidebar() {
     }
   );
 
+  function handleDragStart(e: DragStartEvent) {
+    if (e.active.data.current?.type === "channelGroup") {
+      const activeGroup = channelGroups.find(
+        (channelGroup) => channelGroup.id === e.active.id
+      );
+      setDraggedGroup(activeGroup ?? null);
+    } else if (e.active.data.current?.type === "channel") {
+      const activeChannel = channelGroups
+        .flatMap((channelGroup) => channelGroup.channels)
+        .find((channel) => channel.channelId === e.active.id);
+      setDraggedChannel(activeChannel ?? null);
+    }
+  }
+
+  function handleDragEnd(e: DragEndEvent) {
+    const { active, over } = e;
+    if (!over) return;
+    if (active.data.current?.type === "channelGroup") {
+      const activeGroupId = active.id;
+      const overGroupId = over.id;
+      if (activeGroupId === overGroupId) return;
+
+      setChannelGroups((prev) => {
+        const activeGroup = channelGroups.find(
+          (channelGroup) => channelGroup.id === activeGroupId
+        );
+        const overGroup = channelGroups.find(
+          (channelGroup) => channelGroup.id === overGroupId
+        );
+        if (!activeGroup || !overGroup) return prev;
+        return prev.map((prevGroup) => {
+          if (prevGroup.id === activeGroup.id) {
+            return {
+              ...prevGroup,
+              uiOrder: overGroup.uiOrder
+            };
+          } else if (
+            prevGroup.uiOrder > activeGroup.uiOrder &&
+            prevGroup.uiOrder <= overGroup.uiOrder
+          ) {
+            return {
+              ...prevGroup,
+              uiOrder: prevGroup.uiOrder - 1
+            };
+          } else if (
+            prevGroup.uiOrder < activeGroup.uiOrder &&
+            prevGroup.uiOrder >= overGroup.uiOrder
+          ) {
+            return {
+              ...prevGroup,
+              uiOrder: prevGroup.uiOrder + 1
+            };
+          } else {
+            return prevGroup;
+          }
+        });
+      });
+      setDraggedGroup(null);
+    } else if (active.data.current?.type === "channel") {
+      if (active.id === over.id) return;
+      setChannelGroups((prev) => {
+        const activeGroup = prev.find((group) =>
+          group.channels.some((channel) => channel.channelId === active.id)
+        );
+        const overGroup = prev.find((group) =>
+          group.channels.some((channel) => channel.channelId === over.id)
+        );
+        if (!activeGroup || !overGroup || activeGroup !== overGroup)
+          return prev;
+
+        const activeChannel = activeGroup.channels.find(
+          (channel) => channel.channelId === active.id
+        )!;
+        const overChannel = activeGroup.channels.find(
+          (channel) => channel.channelId === over.id
+        )!;
+
+        return prev.map((prevGroup) => {
+          if (prevGroup.id !== activeGroup.id) {
+            return prevGroup;
+          } else {
+            const newChannels = prevGroup.channels.map((channel) => {
+              if (channel.channelId === active.id) {
+                return { ...channel, uiOrder: overChannel.uiOrder };
+              } else if (
+                channel.uiOrder > activeChannel.uiOrder &&
+                channel.uiOrder <= overChannel.uiOrder
+              ) {
+                return { ...channel, uiOrder: channel.uiOrder - 1 };
+              } else if (
+                channel.uiOrder < activeChannel.uiOrder &&
+                channel.uiOrder >= overChannel.uiOrder
+              ) {
+                return { ...channel, uiOrder: channel.uiOrder + 1 };
+              } else {
+                return channel;
+              }
+            });
+            return { ...prevGroup, channels: newChannels };
+          }
+        });
+      });
+
+      setDraggedChannel(null);
+    }
+  }
+
+  function handleDragOver(e: DragOverEvent) {
+    const { active, over } = e;
+    if (!over) return;
+    if (active.id === over.id) return;
+
+    if (
+      active.data.current?.type === "channel" &&
+      over.data.current?.type === "channel"
+    ) {
+      setChannelGroups((prev) => {
+        const activeGroup = prev.find((group) =>
+          group.channels.some((channel) => channel.channelId === active.id)
+        );
+        const overGroup = prev.find((group) =>
+          group.channels.some((channel) => channel.channelId === over.id)
+        );
+
+        if (!activeGroup || !overGroup || activeGroup.id === overGroup.id)
+          return prev;
+
+        const activeChannel = activeGroup.channels.find(
+          (channel) => channel.channelId === active.id
+        )!;
+        const overChannel = overGroup.channels.find(
+          (channel) => channel.channelId === over.id
+        )!;
+        return prev.map((prevGroup) => {
+          if (prevGroup.id === activeGroup.id) {
+            return {
+              ...prevGroup,
+              channels: prevGroup.channels.filter(
+                (channel) => channel.channelId !== activeChannel.channelId
+              )
+            };
+          } else if (prevGroup.id === overGroup.id) {
+            const newChannels = prevGroup.channels.map((channel) => {
+              if (channel.uiOrder >= overChannel.uiOrder) {
+                return {
+                  ...channel,
+                  uiOrder: channel.uiOrder + 1
+                };
+              } else {
+                return channel;
+              }
+            });
+            newChannels.push({
+              ...activeChannel,
+              uiOrder: overChannel.uiOrder
+            });
+            return { ...prevGroup, channels: newChannels };
+          } else {
+            return prevGroup;
+          }
+        });
+      });
+    }
+  }
+
   return (
     <div
       className={[
@@ -158,24 +345,45 @@ export default function EditableSidebar() {
       ].join(" ")}
     >
       <ServerCard />
-      <nav className={styles["nav"]}>
-        <ul className={styles["nav-list"]}>
-          {sortedChannelGroups.map((group) => (
-            <EditableChannelGroup channelGroup={group} key={group.uiOrder} />
-          ))}
-          <li className={styles["add-group-container"]}>
-            <button
-              className={styles["add-channel"]}
-              onClick={() =>
-                addModal(<AddGroupModal serverId={serverInfo.serverId} />)
-              }
+      <DndContext
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        onDragOver={handleDragOver}
+      >
+        <nav className={styles["nav"]}>
+          <ul className={styles["nav-list"]}>
+            <SortableContext
+              items={groupIds}
+              strategy={verticalListSortingStrategy}
             >
-              <PlusSymbol />
-              <span>Add new group</span>
-            </button>
-          </li>
-        </ul>
-      </nav>
+              {sortedChannelGroups.map((group) => (
+                <EditableChannelGroup
+                  channelGroup={group}
+                  key={group.uiOrder}
+                />
+              ))}
+            </SortableContext>
+            <li className={styles["add-group-container"]}>
+              <button
+                className={styles["add-channel"]}
+                onClick={() =>
+                  addModal(<AddGroupModal serverId={serverInfo.serverId} />)
+                }
+              >
+                <PlusSymbol />
+                <span>Add new group</span>
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <DragOverlay>
+          {draggedGroup ? (
+            <EditableChannelGroup channelGroup={draggedGroup} />
+          ) : draggedChannel ? (
+            <EditableChannelItem channel={draggedChannel} />
+          ) : null}
+        </DragOverlay>
+      </DndContext>
       {call && <VoiceCallControl />}
       <ProfileCard />
     </div>
