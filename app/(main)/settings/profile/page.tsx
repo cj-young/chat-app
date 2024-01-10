1c982b8744e0d0a04ed3bac216a57a37ca62144f
CJ Young <chrjyoung@gmail.com>
2024-01-09 23:39:25 -0500

    Initialize UI for profile settings page

--- app/(main)/settings/profile/page.tsx
+++ app/(main)/settings/profile/page.tsx
@@ -1,14 +1,52 @@
 "use client";
 import MobileNavToggle from "@/components/MobileNavToggle";
+import ProfilePicture from "@/components/ProfilePicture";
+import { useAuthContext } from "@/contexts/AuthContext";
+import EditIcon from "@/public/pen-to-square-solid.svg";
 import styles from "./page.module.scss";
 
 export default function ProfileSettings() {
+  const { profile } = useAuthContext();
+
   return (
     <>
       <div className={styles["title"]}>
         <MobileNavToggle />
         <h2>Profile</h2>
       </div>
+      <section className={styles["profile-info"]}>
+        <div className={styles["names"]}>
+          <div className={styles["info-container"]}>
+            <h3 className={styles["info-subtitle"]}>Display Name</h3>
+            <div className={styles["info"]}>
+              <span className={styles["display-name"]}>
+                {profile.displayName}
+              </span>
+              <button className={styles["edit-button"]}>
+                <EditIcon />
+              </button>
+            </div>
+          </div>
+          <div className={styles["info-container"]}>
+            <h3 className={styles["info-subtitle"]}>Username</h3>
+            <div className={styles["info"]}>
+              <span className={styles["username"]}>{profile.username}</span>
+              <button className={styles["edit-button"]}>
+                <EditIcon />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles["profile-picture-container"]}>
+          <ProfilePicture
+            user={profile}
+            className={styles["profile-picture"]}
+          />
+          <button className={styles["change-profile-picture"]}>
+            Edit Picture
+          </button>
+        </div>
+      </section>
     </>
   );
 }
