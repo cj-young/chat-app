bcb93d7b295414df26f3c9a97fe4c89f0a879c1b
CJ Young <chrjyoung@gmail.com>
2024-01-15 17:40:43 -0500

    Add button to go to settings page in profile card

--- app/(main)/components/ProfileCard/index.tsx
+++ app/(main)/components/ProfileCard/index.tsx
@@ -2,13 +2,20 @@
 import ProfilePicture from "@/components/ProfilePicture";
 import { useAuthContext } from "@/contexts/AuthContext";
 import { useVoiceCall } from "@/contexts/VoiceChatContext";
+import SettingsIcon from "@/public/gear-solid.svg";
 import MutedMicrophoneIcon from "@/public/microphone-slash-solid.svg";
 import MicrophoneIcon from "@/public/microphone-solid-wide.svg";
+import { useRouter } from "next/navigation";
 import styles from "./styles.module.scss";
 
 export default function ProfileCard() {
   const { profile } = useAuthContext();
   const { isMicMuted, toggleMicMuted } = useVoiceCall();
+  const router = useRouter();
+
+  function goToSettings() {
+    router.push("/settings");
+  }
 
   return (
     <div className={styles["profile-card"]}>
@@ -31,6 +38,13 @@ export default function ProfileCard() {
           aria-label={isMicMuted ? "Unmute microphone" : "Mute microphone"}
         >
           {isMicMuted ? <MutedMicrophoneIcon /> : <MicrophoneIcon />}
+        </button>
+        <button
+          className={[styles["button"], styles["settings-button"]].join(" ")}
+          onClick={goToSettings}
+          aria-label="Go to settings"
+        >
+          <SettingsIcon />
         </button>
       </div>
     </div>
