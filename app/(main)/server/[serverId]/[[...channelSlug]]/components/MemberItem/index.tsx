973e4294acbf67c1cd9f0f61810475992dfc0814
CJ Young <chrjyoung@gmail.com>
2024-01-23 23:42:54 -0500

    Add profile modal upon clicking member in members list

--- app/(main)/server/[serverId]/[[...channelSlug]]/components/MemberItem/index.tsx
+++ app/(main)/server/[serverId]/[[...channelSlug]]/components/MemberItem/index.tsx
@@ -1,4 +1,7 @@
+"use client";
 import ProfilePicture from "@/components/ProfilePicture";
+import ProfileModal from "@/components/ProfilePicture/components/ProfileModal";
+import { useUiContext } from "@/contexts/UiContext";
 import { IClientMember } from "@/types/server";
 import styles from "./styles.module.scss";
 
@@ -7,8 +10,14 @@ interface Props {
 }
 
 export default function ServerMemberItem({ member: { user } }: Props) {
+  const { addModal } = useUiContext();
+
+  function handleClick() {
+    addModal(<ProfileModal user={user} />);
+  }
+
   return (
-    <div className={styles["member-item"]}>
+    <div className={styles["member-item"]} onClick={handleClick}>
       <ProfilePicture user={user} status={user.onlineStatus} />
       <span className={styles["member-name"]}>{user.displayName}</span>
     </div>
